7b31c2b3a0da5d132aacdc7de24897d8e998db96
vls <liangvls@gmail.com>
2013-11-07 09:41:32 +0800

    add before_install hook

---
+++ tests/before_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+mkdir -p tmp
+cd tmp
+
+wget https://re2.googlecode.com/files/re2-20131024.tgz -O re2.tgz && tar zxvf re2.tgz && cd re2 && make && sudo make install

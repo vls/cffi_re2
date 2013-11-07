f01fcc61777fd71b5890aa5fc912375feee15330
vls <liangvls@gmail.com>
2013-11-07 11:09:02 +0800

    add so linking

--- tests/before_install.sh
+++ tests/before_install.sh
@@ -5,4 +5,13 @@ cd `dirname $0`
 mkdir -p tmp
 cd tmp
 
-wget https://re2.googlecode.com/files/re2-20131024.tgz -O re2.tgz && tar zxvf re2.tgz && cd re2 && make && sudo make install
+if [[ "`uname -m`" == "x86_64" ]]; then
+    libdir="lib64"
+else
+    libdir="lib"
+fi
+
+echo $libdir
+wget https://re2.googlecode.com/files/re2-20131024.tgz -O re2.tgz && tar zxvf re2.tgz && cd re2 && make && sudo make install && \
+sudo ln -sf /usr/local/lib/libre2.so /usr/${libdir}/libre2.so && \
+sudo ln -sf /usr/local/lib/libre2.so.0 /usr/${libdir}/libre2.so.0

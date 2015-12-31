3afc59167a9623f20170027b0804c96b84dbdb80
Uli Köhler <ulikoehler@online.de>
2015-12-31 15:43:40 +0100

    Fix typo in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 from setuptools import setup, Extension
 from setuptools import find_packages
 
-mod_cre2 = Extension('cffi_re2.e_cre2', sources=['cre2.cpp'], libraries=['re2'],
+mod_cre2 = Extension('cffi_re2._cre2', sources=['cre2.cpp'], libraries=['re2'],
     include_dirs=['/usr/local/include'], extra_compile_args=["-g"],
     extra_link_args=["-g"])
 

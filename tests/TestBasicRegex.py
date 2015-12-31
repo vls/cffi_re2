012b2856d693ad5df218afa22164be2733d75aa6
Uli Köhler <ulikoehler@online.de>
2015-12-31 04:05:41 +0100

    Fix test_sub_chinese in Python2

--- tests/TestBasicRegex.py
+++ tests/TestBasicRegex.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import cffi_re2
-from nose.tools import raises, assert_is_not_none, assert_is_none, assert_equal
+from nose.tools import raises, assert_is_not_none, assert_is_none, assert_equal, assert_true, assert_false
 
 class TestBasicRegex(object):
     def test_basic_search(self):
@@ -40,12 +40,11 @@ class TestChineseRegex(object):
     def test_match_chinese(self):
         robj = cffi_re2.compile('梦[^一-龥]*幻[^一-龥]*西[^一-龥]*游')
 
-        assert robj.search('梦1幻2西3游')
-        assert not robj.search('梦倩女幻幽魂西2游')
+        assert_true(robj.search('梦1幻2西3游'))
+        assert_false(robj.search('梦倩女幻幽魂西2游'))
     def test_sub_chinese(self):
         robj = cffi_re2.compile('梦[^一-龥]*幻[^一-龥]*西[^一-龥]*游')
-
-        assert robj.sub('倩女', '梦幻西游好玩吗?') == '倩女好玩吗?'
+        assert_equal(robj.sub('倩女', '梦幻西游好玩吗?'), u'倩女好玩吗?')
 
     @raises(ValueError)
     def test_invalid_regex(self):

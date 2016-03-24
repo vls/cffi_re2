e57c9cfc38499cc9c7e2de8b8bb424ae394173c2
ukessi <liangvls@gmail.com>
2016-03-24 13:05:05 +0800

    add c++11 compiler option

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, Extension
 from setuptools import find_packages
 
 mod_cre2 = Extension('cffi_re2._cre2', sources=['cre2.cpp'], libraries=['re2'],
-    include_dirs=['/usr/local/include'], extra_compile_args=["-g"],
+    include_dirs=['/usr/local/include'], extra_compile_args=["-g", "-std=c++11"],
     extra_link_args=["-g"])
 
 tests_require=['nose']
